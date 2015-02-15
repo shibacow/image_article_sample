9029dc759befc5956d9b7416504510282e68fd03
shibacow <shibacow@www2326ue.sakura.ne.jp>
2015-02-15 15:53:46 +0900

    layout fixed

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,14 @@
+require 'uri'
+
 module ArticlesHelper
+  def auto_link_to(text)
+    result=[]
+    text.split("\n").each { |line|
+      URI.extract(line,["http","https"]).each do |uri|
+        line=link_to(uri,uri)
+      end
+      result.push(line)
+    }
+    result.join("\n")
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ActiveRecord::Base
   has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  validates :url, {
+    :allow_blank => true,
+    :format => URI::regexp(%w(http https)),
+  }
 end
